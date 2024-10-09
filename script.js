3d75cfc2205a0e2ef7d900a7dcffe16bca992a2f
ryon0010 <131249868+ryon0010@users.noreply.github.com>
2024-10-09 08:52:37 +0800

    add files

---
+++ script.js
@@ -0,0 +1,20 @@
+var vg_1 = "map_vis.json";
+vegaEmbed("#map", vg_1)
+    .then(function (result) {})
+    .catch(console.error);
+
+var vg_2 = "line_vis.json";
+vegaEmbed("#line", vg_2)
+    .then(function (result) {})
+    .catch(console.error);
+    
+var vg_3 = "bar_vis.json";
+vegaEmbed("#bar",vg_3)
+    .then(function (result) {})
+    .catch(console.error);
+
+var vg_4 = "donut_vis.json";
+vegaEmbed("#donut",vg_4)
+    .then(function (result) {})
+    .catch(console.error);
+
